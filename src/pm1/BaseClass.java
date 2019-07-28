e4278ace9f7a1850cd081511377e27782ebcc057
Siddu <sgouti3@gmail.com>
2019-07-28 16:36:13 +0530

    siddubash

--- src/pm1/BaseClass.java
+++ src/pm1/BaseClass.java
@@ -3,6 +3,7 @@ package pm1;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -16,8 +17,8 @@ public class BaseClass {
 	{	
 		AndroidDriver<AndroidElement> Driver;
 		try {
-		File fln=new File("src");
-		File Gtapp=new File(fln,"ApiDemos-debug.apk");
+		File fln=new File("E:\\Automation\\Appium\\Apkfolder");
+		File Gtapp=new File(fln,"naukriApp.apk");
 		DesiredCapabilities Cap=new DesiredCapabilities();
 		
 		Cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Sid1");
@@ -25,7 +26,9 @@ public class BaseClass {
 		Cap.setCapability(MobileCapabilityType.APP,Gtapp.getAbsolutePath());
 		
 		 Driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),Cap);
+		 Driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		 return Driver;
+		 
 	}
 	catch(MalformedURLException e)
 	{
--- src/pm1/M1.java
+++ src/pm1/M1.java
@@ -1,20 +1,31 @@
 package pm1;
 
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 public class M1 extends BaseClass {
-
+	public static AndroidDriver<AndroidElement> Driver;
 	public static void main(String[] args) throws IOException, InterruptedException    {
 		// TODO Auto-generated method stub
-		AndroidDriver<AndroidElement> Driver;
-		Driver=SetupAndrd();
+		
+		Runcmd();
+		//Driver=SetupAndrd();
+	//	NaukriLogin();
+	
+	}
+	public static void APIDemoTest()
+	{
 		Driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		String AppXpath="//android.widget.TextView[@text='Preference']";
 		//Driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
@@ -36,16 +47,68 @@ public class M1 extends BaseClass {
 		{
 			System.out.println("Setting option is failed");
 		}
-	
 	}
 	
+	public static void NaukriLogin()
+	{
+		AndroidElement LoginButton=GetAndrdEleemnt("id","naukriApp.appModules.login:id/b_login");
+		LoginButton.click();
+		
+		AndroidElement EmailRdioBtn=GetAndrdEleemnt("id","naukriApp.appModules.login:id/rd_email");
+		if(EmailRdioBtn.isSelected())
+		{
+			System.out.println("email btn is selected and Button Name:- "+ EmailRdioBtn.toString());
+		}
+		GetAndrdEleemnt("id","naukriApp.appModules.login:id/et_email").sendKeys("sidu@gmail.com");//enter emailid
+		AndroidElement PassFld=	GetAndrdEleemnt("id","naukriApp.appModules.login:id/et_password");
+		PassFld.sendKeys(".sidsss");
+		//enter pass
+			AndroidElement PassHideBtn=	GetAndrdEleemnt("id","naukriApp.appModules.login:id/tv_hide");
+	if(PassHideBtn.isEnabled()&& !PassHideBtn.getText().equalsIgnoreCase("Show"))
+	{
+		//PassHideBtn.click();
+		String ss="\\android.widget.EditText[@passowrd='true']";
+		System.out.println(PassFld.getText());
+	if(	PassFld.getText().startsWith("."))//••
+	{
+		System.out.println("password is not showing");
+	}
+
+		
+		
+	}
+	} 
 	
+	public static AndroidElement GetAndrdEleemnt(String XpathType,String value)
+	{
+		AndroidElement driver=null;
+		
+		switch(XpathType.toUpperCase()) {
+		case "ID":
+			driver=Driver.findElementById(value);
+		break;	
+		case "XPATH":
+			driver=Driver.findElementByXPath(value);
+		break;
+		case "CLASSNAME":
+			driver=Driver.findElementByClassName(value);
+		break;
+		default:
+			System.out.println("No element found");
+			break;
+		}
+		return driver;
+		
+	}
 	public static void Runcmd() throws IOException
 	{
 		Process p = Runtime
                 .getRuntime()
                 .exec("cmd /c start cmd.exe /K \"cd C:\\Users\\siddh\\AppData\\Local\\Android\\Sdk\\emulator &&  emulator -avd Sid1");
-		 SetupAndrd();
+		Process p1 = Runtime
+                .getRuntime()
+                .exec("cmd /c start cmd.exe /K  \"cd C:\\Users\\siddh");
+		
 	}
 
 	
